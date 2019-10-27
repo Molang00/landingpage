9f4ee3af7990f8403636e17401f8a70dc26aef19
molang <bskim00@postech.ac.kr>
2019-10-27 22:15:22 +0900

    add English language

--- src/index.js
+++ src/index.js
@@ -9,24 +9,24 @@ import Footer from "./Footer";
 import "./styles/Main.css";
 
 const messages = {
-	ko: require("./translations/ko.json")
-	// en: require("./translations/en.json")
+  ko: require("./translations/ko.json"),
+  en: require("./translations/en.json")
 };
 
 class App extends React.Component {
-	render() {
-		const locale = navigator.language.split("-")[0];
-		return (
-			<IntlProvider locale={locale} messages={messages[locale]}>
-				<React.Fragment>
-					<NavBar />
-					<Header></Header>
-					<Main></Main>
-					<Footer />
-				</React.Fragment>
-			</IntlProvider>
-		);
-	}
+  render() {
+    const locale = navigator.language.split("-")[0];
+    return (
+      <IntlProvider locale={locale} messages={messages[locale]}>
+        <React.Fragment>
+          <NavBar />
+          <Header></Header>
+          <Main></Main>
+          <Footer />
+        </React.Fragment>
+      </IntlProvider>
+    );
+  }
 }
 
 render(<App />, document.getElementById("root"));
