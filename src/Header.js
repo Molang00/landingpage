4e317b0f3ff122abaeb268e8f57bcab7a59f29db
ppsseo18 <ppsseo18@gmail.com>
2019-10-27 19:29:21 +0900

    change law message to intl message

--- src/Header.js
+++ src/Header.js
@@ -1,59 +1,72 @@
 import React from "react";
+import { FormattedMessage } from "react-intl";
 import "./styles/Header.css";
 import proto from "./styles/product-img-qt.png";
 import { isMobile, isTablet, osName } from "react-device-detect";
 import { Fragment } from "react";
 
 function getDownloadButton() {
-  if (isMobile === true || isTablet === true) {
-    return (
-      <div className="product-download" id="cocktailDownload">
-        {" "}
-        Start with PC{" "}
-      </div>
-    );
-  } else if (osName === "Mac OS") {
-    return (
-      <Fragment>
-        <a
-          href="https://github.com/CherryBoxIDE/cocktail/releases/download/v0.0.0/Cocktail.dmg"
-          className="product-download"
-          id="cocktailDownload"
-        >
-          {" "}
-          Cocktail Download{" "}
-        </a>
-        <br />
-      </Fragment>
-    );
-  } else {
-    return (
-      <div className="product-download" id="cocktailDownload">
-        {" "}
-        Cocktail Landing{" "}
-      </div>
-    );
-  }
+	if (isMobile === true || isTablet === true) {
+		return (
+			<div className="product-download" id="cocktailDownload">
+				{" "}
+				<FormattedMessage
+					id="header.mobile.startWithPC"
+					defaultMessage="Start with PC"
+				/>{" "}
+			</div>
+		);
+	} else if (osName === "Mac OS") {
+		return (
+			<Fragment>
+				<a
+					href="https://github.com/CherryBoxIDE/cocktail/releases/download/v0.0.0/Cocktail.dmg"
+					className="product-download"
+					id="cocktailDownload"
+				>
+					{" "}
+					<FormattedMessage
+						id="header.pc.downloadButton"
+						defaultMessage="Download Cocktail"
+					/>{" "}
+				</a>
+				<br />
+			</Fragment>
+		);
+	} else {
+		return (
+			<div className="product-download" id="cocktailDownload">
+				{" "}
+				<FormattedMessage
+					id="header.pc.downloadButton"
+					defaultMessage="Download Cocktail"
+				/>{" "}
+			</div>
+		);
+	}
 }
 
 function Header(props) {
-  console.log(osName);
-  return (
-    <div className="header product-wrap" id="download">
-      <img
-        src={proto}
-        className={isMobile === true ? "product-img-mobile" : "product-img"}
-        alt="product-img"
-        id="product-img"
-      />
-      <div className="product-description">
-        {getDownloadButton()}
-        <a href="#feature" className="product-about" id="detail">
-          more detail
-        </a>
-      </div>
-    </div>
-  );
+	console.log(osName);
+	return (
+		<div className="header product-wrap" id="download">
+			<img
+				src={proto}
+				className={isMobile === true ? "product-img-mobile" : "product-img"}
+				alt="product-img"
+				id="product-img"
+			/>
+			<div className="product-description">
+				{getDownloadButton()}
+				<a href="#feature" className="product-about" id="detail">
+					<FormattedMessage
+						id="header.pc.moreDetail"
+						defaultMessage="more detail"
+					/>
+				</a>
+			</div>
+		</div>
+	);
 }
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
+import { IntlProvider } from "react-intl";
 
 import Header from "./Header";
 import NavBar from "./NavBar";
@@ -7,17 +8,25 @@ import Main from "./Main";
 import Footer from "./Footer";
 import "./styles/Main.css";
 
+const messages = {
+	ko: require("./translations/ko.json")
+	// en: require("./translations/en.json")
+};
+
 class App extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <NavBar />
-        <Header></Header>
-        <Main></Main>
-        <Footer />
-      </React.Fragment>
-    );
-  }
+	render() {
+		const locale = navigator.language.split("-")[0];
+		return (
+			<IntlProvider locale={locale} messages={messages[locale]}>
+				<React.Fragment>
+					<NavBar />
+					<Header></Header>
+					<Main></Main>
+					<Footer />
+				</React.Fragment>
+			</IntlProvider>
+		);
+	}
 }
 
 render(<App />, document.getElementById("root"));
