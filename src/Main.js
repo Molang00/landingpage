6ff957deaff6fcb5aa90f5311f6e515913256891
molang <bskim00@postech.ac.kr>
2019-11-11 15:30:07 +0900

    change descriptions for prototype

--- src/Main.js
+++ src/Main.js
@@ -32,29 +32,33 @@ function Main(props) {
             <p>
               <FormattedMessage
                 id="main.pc.p1"
-                defaultMessage="지금까지 존재했던 다양한 코드 편집기들은 너무 어려웠습니다. 코드를
-                한 줄이라도 적어보기 위해선 프로젝트를 생성하고, 복잡한 환경변수
-                설정이 필요했죠."
+                defaultMessage="지금까지 존재했던 다양한 코드 편집기들은 처음 코딩을 배우는 분들께 너무 어려웠습니다. 그래서 저희 CherryBox가 Cocktail을 개발했습니다!"
               />
             </p>
+            <ul>
+              <li>
+                <FormattedMessage id="main.pc.ul1" />
+              </li>
+              <p>
+                <FormattedMessage id="main.pc.ul1.p" />
+              </p>
+              <li>
+                <FormattedMessage id="main.pc.ul2" />
+              </li>
+              <p>
+                <FormattedMessage id="main.pc.ul2.p" />
+              </p>
+              <li>
+                <FormattedMessage id="main.pc.ul3" />
+              </li>
+              <p>
+                <FormattedMessage id="main.pc.ul3.p" />
+              </p>
+            </ul>
+            <br />
             <p>
               <FormattedMessage
                 id="main.pc.p2"
-                defaultMessage="그래서 저희 CherryBox가 Cocktail을 개발했습니다!"
-              />
-            </p>
-            <p>
-              <FormattedMessage
-                id="main.pc.p3"
-                defaultMessage="가장 빠르게 C 코딩을 시작할 수 있는 CockTail은 설치 후 프로그램을 실행하면 바로 코딩할 수 있습니다.{br}물론 Cocktail내에서 코드 컴파일 및 실행도 가능하기 때문에 테스트까지 한번에 진행할 수 있습니다."
-                values={{
-                  br: <br />
-                }}
-              />
-            </p>
-            <p>
-              <FormattedMessage
-                id="main.pc.p4"
                 defaultMessage="c언어를 이용해 처음 코딩을 배우시는 분,{br}한 파일만으로 알고리즘 문제를 푸시는 분{br}한 번 써보시면 편리함에 빠져버린 자신을 마주하게 되실거에요."
                 values={{
                   br: <br />
